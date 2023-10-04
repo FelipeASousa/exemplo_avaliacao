5436ec3752b4a886770352a40b29d14d99d70b11
FelipeASousa <feaugusto893@gmail.com>
2023-10-04 20:22:52 -0300

    exemplo avaliacao

---
+++ src/main/java/com/exemplo_avaliacao/exemplo_avaliacao/ExemploAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo_avaliacao.exemplo_avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo_avaliacao/exemplo_avaliacao/ExemploAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo_avaliacao.exemplo_avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
